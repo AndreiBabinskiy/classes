0157c52f244b5472571b62d5652aade03270df82
Your Name <you@example.com>
2022-04-23 18:55:12 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Laba3_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student studIvanov = new Student();
+            studIvanov.NumberGroup = 10321;
+            studIvanov.StudentNumberInTheGroup = 1;
+            studIvanov.LastName = "Ivanov";
+            studIvanov.firtsName = "Oleg";
+            studIvanov.middleName = "Petrovich";
+            studIvanov.ArrayScore = new int[] { 10, 7, 6, 9, 8 };
+            Console.WriteLine(studIvanov);
+            Console.WriteLine($"Average Score: {studIvanov.AverageScore()}");
+            Console.WriteLine("--------------------------------------------------------------");
+            Student studPetrov = new Student(10322, 2, "Petrov", "Aleksey", "Sidorovich");
+            try
+            {
+                studPetrov.ArrayScore = new int[] { 7, 9, 5, 8, 4 };
+                Console.WriteLine(studPetrov);
+                Console.WriteLine($"Average Score: {studPetrov.AverageScore()}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine($"{studIvanov.LastName} more successful, than {studPetrov.LastName} on the {studIvanov.SuccessfulStudent(studPetrov)} points");
+            Console.WriteLine($"{studIvanov.LastName} more successful, than {studPetrov.LastName} \n { studIvanov.SuccessfulStudentBool(studPetrov)} ");
+            Console.WriteLine("-----------------------------------------------------------");
+            Student studSidorov = new Student(10323, 3, "Sidorov", "Andrey", "Viktorovich", new int[] { 5, 6, 5, 3, 7 });
+            Console.WriteLine(studSidorov);
+            Console.WriteLine($"Average Score: {studSidorov.AverageScore()}");
+            /*Student std1 = new Student();
+            Console.WriteLine(std1);*/
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Most successful student of all \n\n" + Student.MostSuccessfulOfAll(studIvanov,studPetrov,studSidorov));
+            Console.WriteLine("------------------------------------------------------------");
+            studIvanov.ArrayScore = new int[] { 10, 7, 6, 3, 8 };
+            studIvanov.CorrectNumberGroup();
+            Console.WriteLine(studIvanov);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba3_Task
+{
+    class Student
+    { 
+        private int numberGroup; //номер группы 
+        private int studentNumberInTheGroup; //номер студента в группе
+        private string lastName; //фамилия
+        public string firtsName; //имя
+        public string middleName; //отчество
+        private int[] arrayScore = new int[5]; //оценки по 5 предметам (массив)
+
+        //public Student() { }
+        public static Student MostSuccessfulOfAll(Student stdOne, Student stdTwo, Student stdThree)
+        {
+            if (stdOne.AverageScore() > stdTwo.AverageScore() & stdTwo.AverageScore() > stdThree.AverageScore())
+            {
+                return stdOne;
+            }
+            if (stdTwo.AverageScore() > stdThree.AverageScore())
+            {
+                return stdTwo;
+            }
+            return stdThree;
+
+        }
+        public int SuccessfulStudent(Student student)
+        {
+            return AverageScore() - student.AverageScore();
+        }
+
+        public Boolean SuccessfulStudentBool(Student student)
+        {
+            double o1 = AverageScore();
+            double o2 = student.AverageScore();
+            return (o1 > o2);
+        }
+
+        public void CorrectNumberGroup()
+        {
+            foreach(int array in ArrayScore) {
+                if (array < 4)
+                {
+                    NumberGroup = 0;
+                }
+            }
+            
+        }
+
+        public int NumberGroup
+        {
+            get
+            {
+                return numberGroup;
+            }
+            set
+            {
+                if(value < 10300 | value > 10400)
+                {
+                    numberGroup = 0;
+                }
+                else
+                {
+                    numberGroup = value;
+                }
+            }
+        }
+
+        //public int StudentNumberInTheGroup { get => studentNumberInTheGroup; set => studentNumberInTheGroup = value; }
+        public int StudentNumberInTheGroup { 
+            get => studentNumberInTheGroup; 
+            set
+            {
+                if (value < 0 | value > 30)
+                {
+                    throw new Exception(" Incorrect student number in the group ");
+                }
+                else
+                {
+                    studentNumberInTheGroup = value;
+                }
+            }
+        }
+
+        public int[] ArrayScore { 
+            get => arrayScore; 
+            set
+            {
+                int j = 0;
+                foreach (int array in value)
+                {
+                    if (array > 0 & array < 11)
+                    {
+                        arrayScore[j++] = array;
+                    }
+                    else
+                    {
+                        arrayScore[j++] = 0;
+                    }
+                    if (j >= 5)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        /* public Student(int n_group, int n_id, string name, string patronymic, string surname, params int[] ocenka) : this(n_group, n_id, name, patronymic, surname)
+        {
+            int j = 0;
+            foreach (int i in ocenka)
+            {
+                this.ocenka[j++] = i;
+                if (j >= 5) break;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                if (ocenka[i] < 0 | ocenka[i] > 10)
+                    throw new Exception($"Неверная оценка у студента {surname}");
+                if (ocenka[i] < 4)
+                {
+                    this.n_group = 0;
+                }
+            }
+
+        }*/
+
+        public string LastName { get => lastName; set => lastName = value; }
+
+        public Student() : this(0,0, "NoLastName", "NoFirstName", "NoMiddleName")
+        { 
+
+        }
+
+        public Student(int numberGroup, int studentNumberInTheGroup, string lastName, string firtsName, string middleName)
+        {
+            NumberGroup = numberGroup;
+            StudentNumberInTheGroup = studentNumberInTheGroup;
+            LastName = lastName;
+            this.firtsName = firtsName;
+            this.middleName = middleName;
+            
+        }
+
+        public Student(int numberGroup, int studentNumberInTheGroup, string lastName, string firtsName, string middleName, params int[] arrayScore) : this(numberGroup, studentNumberInTheGroup, lastName, firtsName, middleName)
+        {
+            ArrayScore = arrayScore;
+        }
+
+        public int AverageScore()
+        {
+            return (ArrayScore[0] + ArrayScore[1] + ArrayScore[2] + ArrayScore[3] + ArrayScore[4]) /5;
+        }
+
+        public override string ToString()
+        {
+            return $"number group: {NumberGroup}\n" +
+                $"student number in the group: {StudentNumberInTheGroup}\n" +
+                $"last name: {LastName}\n" +
+                $"firts name: {firtsName}\n" +
+                $"patronymic: {middleName}\n" +
+                $"arrayScore: {ArrayScore[0]} {ArrayScore[1]} {ArrayScore[2]} {ArrayScore[3]} {ArrayScore[4]}";
+        }
+    }
+}
